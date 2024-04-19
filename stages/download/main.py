acde9d6f9f8117e4ea65b8e1390926b464c1123d
ddomizzisg <ddomizzisag@gmail.com>
2024-04-19 09:40:21 +0200

    script download added

---
+++ stages/download/main.py
@@ -0,0 +1,61 @@
+# Import the API
+from landsatxplore.api import API
+from landsatxplore.earthexplorer import EarthExplorer
+import json
+import concurrent.futures
+
+def download(scenes):
+    for scene in scenes:
+        #print(scene['satellite'])
+        if scene['satellite'] == 8:
+            print(scene['acquisition_date'].strftime('%Y-%m-%d'))
+            print(scene['landsat_product_id'])
+            try:
+                ee.download(scene['landsat_product_id'], output_dir='./data')
+            except:
+                print("Error")
+
+# Initialize the API with your EarthExplorer credentials
+username = ''
+password = ''
+
+api = API(username, password)
+ee = EarthExplorer(username, password)
+
+
+# Search for Landsat TM scenes
+scenes = api.search(
+    dataset='landsat_ot_c2_l1',
+    bbox=(-98.861891, 23.754406, -98.668447, 24.093553),
+    start_date='2010-01-01',
+    end_date='2023-12-01',
+    max_results=10000,
+    max_cloud_cover=20
+)
+
+print(f"{len(scenes)} scenes found.")
+
+sublist_size = scenes // 3
+
+# Divide the list into three sublists
+sublist1 = scenes[:sublist_size]
+sublist2 = scenes[sublist_size: 2 * sublist_size]
+sublist3 = scenes[2 * sublist_size:]
+
+sublists = [sublist1, sublist2, sublist3]
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    results = list(executor.map(process_sublist, sublists))
+    #print(scene['acquisition_date'].strftime('%Y-%m-%d'))
+    # Write scene footprints to disk
+    #fname = f"{scene['landsat_product_id']}.geojson"
+    #with open(fname, "w") as f:
+    #    json.dump(scene['spatial_coverage'].__geo_interface__, f)
+#    print(scene['landsat_product_id'])
+#   try:
+#        ee.download(scene['landsat_product_id'], output_dir='./data')
+#    except:
+#        print("Error")
+
+
+api.logout()
